e4d2c84e4072eec7e65094505303b62c2fb7fea3
AubreyV <breyvicente@gmail.com>
2019-09-30 16:05:28 +0800

    Login Page View

---
+++ resources/js/Employees/pages/LoginPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="d-flex h-100">
+    <div class="d-flex">
+      <div class="d-flex flex-column flex-even text-center carousel-container">
+        <Carousel></Carousel>
+        <div class="quote-of-the-day p-5">"Work smarter, not harder" - Carl Barks</div>
+      </div>
+      <div class="d-flex flex-column flex-even justify-content-center login-form">
+        <form class="p-5" action>
+          <div class="form-group">
+            <input type="text" class="form-control mx-auto w-50" placeholder="user@example.com" />
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control mx-auto w-50" placeholder="******" />
+          </div>
+          <div class="form-group pt-4 text-center">
+            <button class="btn btn-secondary" type="submit">Login</button>
+          </div>
+          <div class="form-group text-center text-center">
+            <button class="btn btn-default">QR Scan</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from "../components/Carousel";
+
+export default {
+  components: {
+    Carousel
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../../../sass/_init.scss";
+
+.login-form {
+  background: $primary-color;
+}
+
+.carousel-container {
+  background-color: $secondary-color;
+
+  .quote-of-the-day {
+    color: white;
+    font-style: italic;
+  }
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import LoginPage from './Employees/pages/LoginPage';
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        'login-page': LoginPage,
+    }
+});
