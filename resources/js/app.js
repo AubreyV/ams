2190a4048316b9f33d4b68cf8291d1c6fe40c697
AubreyV <breyvicente@gmail.com>
2019-09-30 17:44:29 +0800

    Login Page View

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import LoginPage from './Employees/pages/LoginPage'
+
+require('./bootstrap')
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    'login-page': LoginPage
+  }
+})
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
