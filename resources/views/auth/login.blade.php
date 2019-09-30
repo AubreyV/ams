efb749da02bbe6e1170ac363719c0ca7aff6a83d
AubreyV <breyvicente@gmail.com>
2019-09-30 17:44:29 +0800

    Login Page View

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/pages/login_page.css') }}">
+@endsection
+
+@section('content')
+    <login-page></login-page>
+@endsection
